fac907db7f06d1da203e32ceb1a82daa5e4eb230
Denis Davydov <denis6davydov23@gmail.com>
2023-02-01 00:29:11 +0300

    add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import {BuildOptions} from "./types/config";
+import webpack from "webpack";
+import {buildLoaders} from "./buildLoaders";
+import {buildPlugins} from "./buildPlugins";
+import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const {paths, mode} = options;
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
+        },
+        module: {
+            rules: buildLoaders(options)
+        },
+        devtool: options.isDev ? 'inline-source-map' : undefined,
+        plugins: buildPlugins(options),
+        resolve: buildResolvers(),
+        devServer: options.isDev ? buildDevServer(options) : undefined,
+    };
+}
