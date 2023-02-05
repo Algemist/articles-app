0af3c849ebf7c04072df2c11ddee20d6663b56a1
denyoror <denis6davydov23@gmail.com>
2023-02-05 18:07:03 +0300

    add NavBar with themes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,27 +3,17 @@ import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/route";
+import {Navbar} from "widgets/Navbar";
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 const App = () => {
-    const {theme, handleTheme} = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={handleTheme}>switch theme</button>
-            <Link to={'/'}>
-                Main page link
-            </Link>
-            <Link to={'/about'}>
-                About page
-            </Link>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Routes>
-                        <Route path={'/about'} element={<AboutPage />}/>
-                        <Route path={'/'} element={<MainPage />}/>
-                    </Routes>
-                </Suspense>
+            <Navbar />
+            <AppRouter />
         </div>
     );
 };
--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -2,17 +2,17 @@ import {useContext, useMemo, useState} from "react";
 import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./ThemeContext";
 
 interface useThemeResult {
-    handleTheme: () => void;
+    toggleTheme: () => void;
     theme: Theme;
 }
 export function useTheme(): useThemeResult {
     const {theme, setTheme} = useContext(ThemeContext);
 
-    const handleTheme = () => {
+    const toggleTheme = () => {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
         setTheme(newTheme);
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
 
-    return {theme, handleTheme}
+    return {theme, toggleTheme}
 }
