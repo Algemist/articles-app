de3b260174c549c3f5b4bba98d13a4b07249836c
Denis Davydov <denis6davydov23@gmail.com>
2023-04-19 19:46:58 +0300

    fix article list, remove dynamic list

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -16,9 +16,10 @@ interface ArticleListProps {
     view?: ArticleView;
     target?: HTMLAttributeAnchorTarget;
     virtualized?: boolean;
+    onEndReached?: () => void;
 }
 
-const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.SMALL ? 9 : 3)
+const getSkeletons = (view: ArticleView) => new Array(3)
     .fill(0)
     .map((item, index) => (
         <ArticleListItemSkeleton
@@ -36,44 +37,10 @@ export const ArticleList = memo((props: ArticleListProps) => {
         view = ArticleView.SMALL,
         target,
         virtualized = true,
+        onEndReached,
     } = props;
 
     const { t } = useTranslation();
-
-    const isBig = view === ArticleView.BIG;
-
-    const itemsPerRow = isBig ? 1 : 3;
-    const rowCount = isBig ? articles.length : Math.ceil(articles.length / itemsPerRow);
-
-    const rowRenderer = ({
-        index, key, style,
-    }: any) => {
-        const items = [];
-        const fromIndex = index * itemsPerRow;
-        const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
-
-        for (let i = fromIndex; i < toIndex; i += 1) {
-            items.push(
-                <ArticleListItem
-                    article={articles[i]}
-                    view={view}
-                    className={cls.card}
-                    target={target}
-                    key={articles[i].id}
-                />,
-            );
-        }
-
-        return (
-            <div
-                key={key}
-                style={style}
-                className={cls.row}
-            >
-                {items}
-            </div>
-        );
-    };
 
     if (!isLoading && !articles.length) {
         return (
@@ -87,7 +54,7 @@ export const ArticleList = memo((props: ArticleListProps) => {
         );
     }
 
-    if (!virtualized) {
+    if (!virtualized || !onEndReached) {
         return (
             <div
                 className={classNames(cls.ArticleList, {}, [className, cls[view]])}
@@ -117,12 +84,10 @@ export const ArticleList = memo((props: ArticleListProps) => {
         >
             {view === ArticleView.BIG ? (
                 <Virtuoso
-                    style={{ height: '100%' }}
                     useWindowScroll
-                    totalCount={articles.length}
                     data={articles}
                     itemContent={renderListItem}
-
+                    endReached={onEndReached}
                 />
             ) : (
                 <VirtuosoGrid
@@ -130,45 +95,7 @@ export const ArticleList = memo((props: ArticleListProps) => {
 
                 />
             )}
-            {isLoading && getSkeletons(view)}
+            {/* {isLoading && getSkeletons(view)} */}
         </div>
     );
-
-    // return (
-    //     <WindowScroller
-    //         scrollElement={document.getElementById(PAGE_ID) as Element}
-    //     >
-    //         {({
-    //             width,
-    //             height,
-    //             registerChild,
-    //             onChildScroll,
-    //             isScrolling,
-    //             scrollTop,
-    //         }) => (
-    //             <div
-    //                 ref={registerChild}
-    //                 className={classNames(cls.ArticleList, {}, [className, cls[view]])}
-    //             >
-    //                 {virtualized ? (
-    //                     <List
-    //                         autoHeight
-    //                         height={height ?? 700}
-    //                         rowCount={rowCount}
-    //                         rowHeight={isBig ? 700 : 330}
-    //                         rowRenderer={rowRenderer}
-    //                         width={width ? width - 80 : 700}
-    //                         onScroll={onChildScroll}
-    //                         isScrolling={isScrolling}
-    //                         scrollTop={scrollTop}
-    //                     />
-    //                 ) : (
-    //
-    //                     ))
-    //                 )}
-    //                 {isLoading && getSkeletons(view)}
-    //             </div>
-    //         )}
-    //     </WindowScroller>
-    // );
 });
